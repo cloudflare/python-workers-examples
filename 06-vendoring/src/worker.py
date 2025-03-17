f5967ab85285e9ef10eae7d0a59266e741adc203
Mike Nomitch <mnomitch@cloudflare.com>
2025-03-17 10:48:30 -0700

    Adds vendored packages

---
+++ 06-vendoring/src/worker.py
@@ -0,0 +1,35 @@
+import jinja2
+from fastapi import FastAPI, Request
+
+environment = jinja2.Environment()
+template = environment.from_string("Hello, {{ name }}!")
+
+
+async def on_fetch(request, env):
+    import asgi
+
+    return await asgi.fetch(app, request, env)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    message = "This is an example of FastAPI with Jinja2 - go to /hi/<name> to see a template rendered"
+    return {"message": message}
+
+
+@app.get("/hi/{name}")
+async def say_hi(name: str):
+    message = template.render(name=name)
+    return {"message": message}
+
+
+@app.get("/env")
+async def env(req: Request):
+    env = req.scope["env"]
+    return {
+        "message": "Here is an example of getting an environment variable: "
+        + env.MESSAGE
+    }
