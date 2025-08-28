3872b66cb686bec34b8fbbd908e6ab7b570195f3
Hood Chatham <roberthoodchatham@gmail.com>
2025-08-28 12:31:46 +0200

    Add pre-commit config

--- 04-query-d1/db_init.sql
+++ 04-query-d1/db_init.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS qtable ( 
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    quote TEXT, 
+CREATE TABLE IF NOT EXISTS qtable (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quote TEXT,
     author Text
 );
 
-INSERT INTO qtable (quote, author) 
+INSERT INTO qtable (quote, author)
 SELECT * FROM (
-    VALUES 
+    VALUES
         ("This is a very cool quote", "Hood Chatham"),
         ("Some words of wisdom", "Dominik Picheta"),
         ("The sense 'fragment of verbal expression', attested from the 17th century", "Wikipedia")
